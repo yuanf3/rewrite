370fc859d1262a29e3508e3961c21751c41af7b2
Fei <yuanfeitt@gmail.com>
2026-03-16 00:31:05 +1100

    FE: apply prettier

--- frontend/src/components/ui/scroll-area.tsx
+++ frontend/src/components/ui/scroll-area.tsx
@@ -1,7 +1,7 @@
-import * as React from "react"
-import { ScrollArea as ScrollAreaPrimitive } from "radix-ui"
+import { ScrollArea as ScrollAreaPrimitive } from "radix-ui";
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function ScrollArea({
   className,
@@ -23,7 +23,7 @@ function ScrollArea({
       <ScrollBar />
       <ScrollAreaPrimitive.Corner />
     </ScrollAreaPrimitive.Root>
-  )
+  );
 }
 
 function ScrollBar({
@@ -47,7 +47,7 @@ function ScrollBar({
         className="relative flex-1 rounded-full bg-border"
       />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
-  )
+  );
 }
 
-export { ScrollArea, ScrollBar }
+export { ScrollArea, ScrollBar };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,5 +13,5 @@ createRoot(document.getElementById("root")!).render(
         <App />
       </TooltipProvider>
     </ThemeProvider>
-  </StrictMode>,
+  </StrictMode>
 );
